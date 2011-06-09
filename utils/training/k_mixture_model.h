f62f97a9ec2f78ce77fce9cfad46e77dadfad290
Peng Wu <alexepico@gmail.com>
2011-06-09 10:40:27 +0800

    refine code in k mixture model header

--- utils/training/k_mixture_model.h
+++ utils/training/k_mixture_model.h
@@ -120,11 +120,11 @@ typedef struct{
 typedef struct{
     /* dummy varibles */
     guint32 dummy[3];
-    /* the freq of uni-gram. see m_total_freq in magic header also. */
-    guint32 m_freq;
-    guint32 dummy2[3];
     /* the total number of instances of word W1. */
     guint32 m_WC;
+    guint32 dummy2[3];
+    /* the freq of uni-gram. see m_total_freq in magic header also. */
+    guint32 m_freq;
 } KMixtureModelArrayHeader;
 
 typedef struct{
