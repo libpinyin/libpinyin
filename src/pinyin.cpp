546f675be708b4062942110e9fcf07851e4457fa
Peng Wu <alexepico@gmail.com>
2011-07-25 13:56:49 +0800

    add notes

--- src/pinyin.cpp
+++ src/pinyin.cpp
@@ -1,3 +1,7 @@
 #include "pinyin.h"
 
 /* Place holder for combining static libraries in sub-directories. */
+
+/* Note: will implement a glue layer for input method integration.
+ * Refer to: fcitx-sunpinyin
+ */
