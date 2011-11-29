192b5d79a24c36f0c1247bf837e2e837098145f6
Peng Wu <alexepico@gmail.com>
2011-11-29 15:51:22 +0800

    refactor test cases

--- tests/storage/test_phrase_table.cpp
+++ tests/storage/test_phrase_table.cpp
@@ -1,27 +1,8 @@
+#include "timer.h"
 #include <string.h>
-#include <stdio.h>
-#include <sys/time.h>
 #include "pinyin_internal.h"
 
 size_t bench_times = 1000;
-
-guint32 record_time ()
-{
-    timeval tv;
-    gettimeofday(&tv, NULL);
-    return (guint32) tv.tv_sec * 1000000 + tv.tv_usec;
-}
-
-void print_time (guint32 old_time, guint32 times)
-{
-    timeval tv;
-    gettimeofday (&tv, NULL);
-
-    guint32 wasted = (guint32) tv.tv_sec * 1000000 + tv.tv_usec - old_time;
-
-    printf("Spent %d us for %d operations, %f us/op, %f times/s.\n\n", wasted, times, ((double)wasted)/times, times * 1000000.0/wasted);
-}
-
 
 int main(int argc, char * argv[]){
     PhraseLargeTable largetable;
---
+++ tests/timer.h
@@ -0,0 +1,48 @@
+/* 
+ *  libpinyin
+ *  Library to deal with pinyin.
+ *  
+ *  Copyright (C) 2011 Peng Wu <alexepico@gmail.com>
+ *  
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <sys/time.h>
+#include <stdio.h>
+#include <glib.h>
+
+
+static guint32 record_time ()
+{
+    timeval tv;
+    gettimeofday (&tv, NULL);
+    return (guint32) tv.tv_sec * 1000000 + tv.tv_usec;
+}
+
+static void print_time (guint32 old_time, guint32 times)
+{
+    timeval tv;
+    gettimeofday (&tv, NULL);
+
+    guint32 wasted = (guint32) tv.tv_sec * 1000000 + tv.tv_usec - old_time;
+
+    printf("Spent %d us for %d operations, %f us/op, %f times/s.\n\n" , wasted , times , ((double) wasted)/times , times * 1000000.0/wasted );
+}
+
+
+#endif
