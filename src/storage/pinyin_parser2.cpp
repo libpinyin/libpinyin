745e87ade00dd7133613a923d7f14f3f8eb5ef97
Peng Wu <alexepico@gmail.com>
2012-05-25 15:22:38 +0800

    fixes parse one key in double pinyin parser

--- src/storage/pinyin_parser2.cpp
+++ src/storage/pinyin_parser2.cpp
@@ -623,6 +623,7 @@ bool DoublePinyinParser2::parse_one_key(pinyin_option_t options,
                                         ChewingKey & key,
                                         const char *str, int len) const {
     options &= ~(PINYIN_CORRECT_ALL|PINYIN_AMB_ALL);
+    options |= PINYIN_CORRECT_UE_VE;
 
     if (1 == len) {
         if (!(options & PINYIN_INCOMPLETE))
