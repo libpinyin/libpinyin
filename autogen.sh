ec6e09e705e570236fbaf21b3acf9d30e6fa2381
Peng Wu <alexepico@gmail.com>
2012-06-21 11:13:55 +0800

    update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,30 @@
-#!/bin/bash
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
 
-aclocal
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
 
-libtoolize --force
+PKG_NAME="libpinyin"
 
-autoheader
+(test -f $srcdir/configure.ac \
+  && test -f $srcdir/README ) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
 
-automake -a
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from the GNOME CVS"
+    exit 1
+}
 
-autoconf
+(test -f $srcdir/ChangeLog) || {
+    touch $srcdir/ChangeLog
+}
 
-./configure --enable-tests
+CFLAGS=${CFLAGS-"-Wall -Werror"}
+
+ACLOCAL_FLAGS="$ACLOCAL_FLAGS"
+REQUIRED_AUTOMAKE_VERSION=1.8
+
+. gnome-autogen.sh $@
