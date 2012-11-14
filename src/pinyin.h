6c7aa5f3d4036318b3d977e47de2b49d0ad0af58
Peng Wu <alexepico@gmail.com>
2012-11-14 15:33:48 +0800

    bump version 0.8.0

--- src/pinyin.h
+++ src/pinyin.h
@@ -506,7 +506,7 @@ typedef ChewingKeyVector PinyinKeyVector;
 typedef ChewingKeyRestVector PinyinKeyPosVector;
 
 
-#define LIBPINYIN_FORMAT_VERSION  "0.7.92"
+#define LIBPINYIN_FORMAT_VERSION  "0.8.0"
 
 };
 
