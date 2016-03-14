db0aa4dec5793e99a3405368565b5f4f0e05ee64
Peng Wu <alexepico@gmail.com>
2016-03-14 13:59:41 +0800

    fixes a typo

--- src/storage/phrase_large_table3_kyotodb.cpp
+++ src/storage/phrase_large_table3_kyotodb.cpp
@@ -19,7 +19,6 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#include "phrase_large_table3_kyotodb.h"
 #include "phrase_large_table3.h"
 #include <kchashdb.h>
 #include <kcprotodb.h>
