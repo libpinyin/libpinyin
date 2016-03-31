3e1537b4c456d9f41bbd81f1f9c00d6920a8137e
Peng Wu <alexepico@gmail.com>
2016-03-31 11:09:37 +0800

    update pinyin_internal.h

--- src/pinyin_internal.cpp
+++ src/pinyin_internal.cpp
@@ -2,6 +2,8 @@
 
 
 /* deprecated headers, just keep it here for compiling. */
+#include "chewing_large_table.h"
+#include "phrase_large_table2.h"
 #include "facade_chewing_table.h"
 #include "facade_phrase_table2.h"
 
