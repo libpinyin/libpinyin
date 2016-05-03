8a86d87ed0247ea5fd041702c12f2980818cdd98
Peng Wu <alexepico@gmail.com>
2016-05-03 14:59:24 +0800

    add phonetic_key_matrix.h

---
+++ src/storage/phonetic_key_matrix.h
@@ -0,0 +1,29 @@
+/* 
+ *  libpinyin
+ *  Library to deal with pinyin.
+ *  
+ *  Copyright (C) 2016 Peng Wu <alexepico@gmail.com>
+ *  
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef PHONETIC_KEY_MATRIX_H
+#define PHONETIC_KEY_MATRIX_H
+
+template<struct Item>
+class PhoneticTable {
+};
+
+#endif
