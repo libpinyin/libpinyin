91de72879019d2461330dbcd16991f1b73d41f82
Peng Wu <alexepico@gmail.com>
2017-01-19 15:57:58 +0800

    drop struct matrix_step

--- src/lookup/phonetic_lookup_linear.h
+++ src/lookup/phonetic_lookup_linear.h
@@ -69,49 +69,4 @@ public:
     }
 };
 
-#if 0
-template <gint32 nbest>
-struct matrix_step {
-private:
-    gint32 m_nelem;
-    matrix_value_t m_elements[nbest];
-
-public:
-    matrix_step(){
-        m_nelem = 0;
-        /* always assume non-used m_elements contains random data. */
-    }
-
-public:
-    gint32 length() { return m_nelem; }
-    const matrix_value_t * begin() { return m_elements; }
-    const matrix_value_t * end() { return m_elements + m_nelem; }
-
-    /* return true if the item is stored into m_elements. */
-    bool eval_item(const matrix_value_t * item) {
-        /* still have space */
-        if (m_nelem < nbest) {
-            m_elements[m_nelem] = *item;
-            m_nelem ++;
-            return true;
-        }
-
-        /* find minium item */
-        matrix_value_t * min = m_elements;
-        for (gint32 i = 1; i < m_nelem; ++i) {
-            if (min->m_poss > m_elements[i].m_poss)
-                min = m_elements + i;
-        }
-
-        /* compare new item */
-        if (item->m_poss > min->m_poss) {
-            *min = *item;
-            return true;
-        }
-
-        return false;
-    }
-};
-#endif
-
 #endif
