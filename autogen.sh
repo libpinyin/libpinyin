01790e725a92457cfaa2ef95f2687dafbbfeae56
Martin Blanchard <tchaik@gmx.com>
2019-02-17 17:18:13 +0000

    prefer autoreconf over gnome-common
    The gnome-common package is deprecated:
    
        https://wiki.gnome.org/Projects/GnomeCommon/Migration

--- autogen.sh
+++ autogen.sh
@@ -1,30 +1,50 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
+test -n "$srcdir" || srcdir=$(dirname "$0")
+test -n "$srcdir" || srcdir=.
+
+olddir=$(pwd)
+
+cd "$srcdir"
 
 PKG_NAME="libpinyin"
 
-(test -f $srcdir/configure.ac \
-  && test -f $srcdir/README ) || {
+(test -f configure.ac && test -f README ) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
 }
 
-which gnome-autogen.sh || {
-    echo "You need to install gnome-common from the GNOME CVS"
-    exit 1
+if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
+        echo "*** WARNING: I am going to run 'configure' with no arguments." >&2
+        echo "*** If you wish to pass any to it, please specify them on the" >&2
+        echo "*** '$0' command line." >&2
+        echo "" >&2
+fi
+
+autoreconf --verbose --force --install || exit 1
+
+(test -f ChangeLog) || {
+    touch ChangeLog
 }
 
-(test -f $srcdir/ChangeLog) || {
-    touch $srcdir/ChangeLog
-}
+cd "$olddir"
 
 CFLAGS=${CFLAGS-"-Wall -Werror"}
 
 ACLOCAL_FLAGS="$ACLOCAL_FLAGS"
 REQUIRED_AUTOMAKE_VERSION=1.8
 
-. gnome-autogen.sh "$@"
+cd "$olddir"
+if [ "$NOCONFIGURE" = "" ]; then
+        $srcdir/configure "$@" || exit 1
+
+        if [ "$1" = "--help" ]; then
+                exit 0
+        else
+                echo "Now type 'make' to compile $PKG_NAME" || exit 1
+        fi
+else
+        echo "Skipping configure process."
+fi
